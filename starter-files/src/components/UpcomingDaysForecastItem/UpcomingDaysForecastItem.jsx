aae350a6c9a5c55a3b83ee8b8eb6adde41206adb
Aamjit <aayanglem@gmail.com>
2022-04-15 02:48:19 +0530

    initial

--- starter-files/src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
+++ starter-files/src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -6,7 +6,7 @@ const IMG_URL = 'https://www.metaweather.com/static/img/weather';
 
 const UpcomingDaysForecastItem = ({ weekday, imgUrl, temperature }) => (
     <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}>
-        <img width="30" className="mb-2" s rc={`${IMG_URL}/${imgUrl}.svg`} alt="" />
+        <img width="30" className="mb-2" src={`${IMG_URL}/${imgUrl}.svg`} alt="" />
         <span className="mb-2">{weekday}</span>
         <span className="font-weight-bold">{temperature}&deg;</span>
     </li>
